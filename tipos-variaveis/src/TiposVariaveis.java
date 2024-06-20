39d07157e0ffeb54a4f9c6bcfa17f130104102b2
wessan001 <wesleyortegassan@gmail.com>
2024-06-20 20:01:18 -0300

    aprendendo sobre tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1412;
+
+        String meuNome = "wesley sanches";
+
+        short numeroCurto = 3;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double valorDePi = 3.14;
+
+        valorDePi = 11.45;
+
+        System.out.println(valorDePi);
+
     }
 }
